324f546b41023c286cbeeffaa03a5f6dcda48c82
Lloyd Lee <kinglloydlee@gmail.com>
2018-05-02 21:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  var two = 2;
+  return n + two;
 }
+
 
 var funkyFunction = function() {
   return function() {
